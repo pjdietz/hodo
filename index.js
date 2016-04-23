c929a684cace6093907eb9828270b0a62e918d0c
PJ Dietz <pjdietz@gmail.com>
2016-04-23 11:46:34 -0400

    Parse basic CLI arguments

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 #!/usr/bin/env node
 
-console.log('Hodor.');
+const commander = require('commander');
+const version = require('./package').version;
+
+commander
+    .version(version)
+    .arguments('<cmd...>');
+commander.parse(process.argv);
+
+const args = process.argv.slice(2);
+if (args.length < 2) {
+    commander.help();
+}
+
+console.log(args);
