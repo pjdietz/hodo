0a4d49b09d034b30b0552b885acd6ee7c5fd7d97
PJ Dietz <pjdietz@gmail.com>
2016-04-23 12:11:00 -0400

    Spawns a child process and outputs "Hodor." on success, "HODOR!" on error.

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 #!/usr/bin/env node
 
+'use strict';
+
+const child_process = require('child_process');
 const commander = require('commander');
 const version = require('./package').version;
 
 commander
     .version(version)
-    .arguments('<cmd...>');
+    .usage('[options] <cmd...>');
 commander.parse(process.argv);
 
 const args = process.argv.slice(2);
@@ -14,3 +17,13 @@ if (args.length < 2) {
 }
 
 console.log(args);
+
+let proc = child_process.spawn(args[0], args.slice(1));
+proc.on('close', (code) => {
+    if (code !== 0) {
+        console.error('HODOR!');
+    } else {
+        console.log('Hodor.');
+    }
+});
+
